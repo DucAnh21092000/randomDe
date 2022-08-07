892cd5e5e833d41b9d04b65f697e62a86bbedbb8
DucAnh21092000 <dinhanh21092000@gmail.com>
2022-08-07 16:05:36 +0700

    gutboiz thai binh

---
+++ js/dethi.js
@@ -0,0 +1,21 @@
+ const  data = [
+    {
+        id:1,
+        answer: ['20182330', '20182331' ,"20182332","20182333"],
+        question:"MSSV của gutboiz Thái Bình là gì ?",
+        correct:'20182330'
+    },
+    {
+        id: 2,
+        answer: ['20182486', '20182487', "20182488", "20182489"],
+        question: "MSSV của fukboiz Hải Dương là gì ?",
+        correct: '20182486'
+    },
+    {
+        id: 2,
+        answer: ['Đức Anh', 'sdsdvdsvds'],
+        question: "Trang giấy trắng là ai ?",
+        correct: 'Đức Anh'
+    }
+]
+export default data
